3be613be8eb6b811939ac947b502c53210539178
Kyrian Obikwelu <Koshnawaza@gmail.com>
2022-02-28 07:47:11 -0500

    setup queue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->command('queue:restart')
+            ->everyFiveMinutes();
+
+        $schedule->command('queue:work --max-time=60')
+            ->everyMinute()
+            ->withoutOverlapping()
+            ->sendOutputTo(storage_path('/logs/queue-jobs.log'));
     }
 
     /**
@@ -25,7 +33,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
